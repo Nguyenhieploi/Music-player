9955489f5174cbb5c9460d9342751118df2bfb77
Nguyen Hiep Loi <loi.nguyen@HBG.COM>
2022-05-05 14:49:38 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,88 @@
+let allMusic = [
+  {
+    name: "Ít Nhưng Dài Lâu Remix",
+    artist: "Hiệp Lợi",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Đám Cưới Nha",
+    artist: "Hồng Thanh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Anh Nên Yêu Cô Ấy Remix",
+    artist: "Mie",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Câu Hẹn Câu Thề Remix",
+    artist: "Đình Dũng",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Ngôi Nhà Hoa Hồng Remix",
+    artist: "Vicky Nhung",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Như Một Người Dưng Remix",
+    artist: "Hoàng Tử Gio",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Benzon",
+    artist: "Tiktok ",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Ông Bà Gìa Tao Lo Hết Remix",
+    artist: "Bình Vàng",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Paper Short Love Remix",
+    artist: "Bill Gates",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Thương Em Lắm",
+    artist: "Tân Trề ",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Yêu Là Cưới",
+    artist: "Tấn Phát ",
+    img: "music-11",
+    src: "music-11"
+  },
+ 
+  {
+    name: "Bao Giờ Kết Hôn",
+    artist: "Tấn Kiệt",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Chọn Ngày Cưới Em",
+    artist: "Tấn Phát ",
+    img: "music-13",
+    src: "music-13"
+  },
+   {
+    name: "Daddy Cool",
+    artist: "David Modny ",
+    img: "music-14",
+    src: "music-14"
+  }, 
+  
+];
